2a1fbefe1806eba62cf2a8f3f9e2aea0948702bc
marta873 <marta873@student.liu.se>
2022-04-13 07:51:16 +0200

    drawing x an circles works now

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ blue = pygame.Color('blue')
 print(white, black)
 pygame.init()
 
+LINEWIDTH = 10
+
 # screen = pygame.display.set_mode([500, 500])
 
 image = pygame.image.load(r'C:\Users\marc\Downloads\tic_tac_toe_X.jpg')
@@ -22,17 +24,26 @@ running = True
 display_surface.fill(white)
 #draw tictactoe board
 def drawtictacboard():
-	pygame.draw.line(display_surface, black, [125,0], [125, 380], 20)
-	pygame.draw.line(display_surface, black, [255,0], [255, 400], 20)
-	pygame.draw.line(display_surface, black, [0, 125], [400, 125], 20)
-	pygame.draw.line(display_surface, black, [0, 255], [400, 255], 20)
+	pygame.draw.line(display_surface, black, [125,0], [125, 380], LINEWIDTH)
+	pygame.draw.line(display_surface, black, [255,0], [255, 400], LINEWIDTH)
+	pygame.draw.line(display_surface, black, [0, 125], [400, 125], LINEWIDTH)
+	pygame.draw.line(display_surface, black, [0, 255], [400, 255], LINEWIDTH)
 
 drawtictacboard()
 
 def draw_circle(row, column):
-	pygame.draw.circle(display_surface, red, (200,200), 55)
+	pygame.draw.circle(display_surface, red, (60 + row*(LINEWIDTH + 120), 60 + column*(LINEWIDTH + 120)), 55)
 	
-# draw_circle(0,0)
+draw_circle(1,1)
+
+def draw_x(row, column):
+	#first draw an x on the first block
+	offset_x = row * (120 + LINEWIDTH)
+	offset_y = column * (120 + LINEWIDTH)
+	pygame.draw.line(display_surface, blue, [5 + offset_x, 5 + offset_y], [115+ offset_x, 115 + offset_y], 5)
+	pygame.draw.line(display_surface, blue, [5 + offset_x, 115 + offset_y], [115 + offset_x , 5 + offset_y], 5)
+
+draw_x(1,2)
 
 while running:
 	# display_surface.blit(image, (0, 0))
@@ -43,11 +54,7 @@ while running:
 			running = False
 		
 		pygame.display.update()
-	
-	# screen.fill((225, 225, 225))
-	#screen,
-
-	# pygame.draw.circle(screen, (0, 0, 225), (250, 250), 75)
+	# print(pygame.mouse.get_pos())
 	pygame.display.flip()
 
 
